2eb00cb8d9400858f36deeecd207f714d3806366
xiuxiu62 <jacremer@live.com>
2023-02-07 02:13:08 -0800

    fix xtask dist from non workspace root

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,8 +1,9 @@
-use std::process::Command;
+use std::{path::PathBuf, process::Command};
 use xtask_wasm::{anyhow, clap, default_dist_dir};
 
 const APP_NAME: &'static str = "scrabble";
 const PACKAGE_NAME: &'static str = "scrabble-ui";
+const STATIC_CONTENT_PATH: &'static str = "scrabble-ui/static";
 
 #[derive(clap::Parser)]
 enum Opt {
@@ -11,18 +12,27 @@ enum Opt {
     Start(xtask_wasm::DevServer),
 }
 
+// Cannonicalizes a path, relative to workspace manifest
+fn cannonicalize_path(relative_path: &str) -> PathBuf {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.pop();
+    path.push(relative_path);
+
+    path
+}
+
 fn main() -> anyhow::Result<()> {
     let opt: Opt = clap::Parser::parse();
+    env_logger::init();
 
     match opt {
         Opt::Dist(dist) => {
             log::info!("Generating package...");
+            let static_content_path = cannonicalize_path(STATIC_CONTENT_PATH);
 
-            dist
-                // .dist_dir_path("dist")
-                .static_dir_path("scrabble-ui/static")
-                .app_name(APP_NAME)
+            dist.app_name(APP_NAME)
                 .run_in_workspace(true)
+                .static_dir_path(static_content_path)
                 .run(PACKAGE_NAME)?;
 
             Ok(())
